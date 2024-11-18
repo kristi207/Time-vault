60cff3f1ba4eb5b1ad2f813aaeea3729073e4f3b
floatingpony <karkideepshova@gmail.com>
2024-11-18 13:04:26 +0545

    about section

--- TimeVaultProject/urls.py
+++ TimeVaultProject/urls.py
@@ -20,7 +20,9 @@ from django.conf import  settings
 from django.conf.urls.static import static
 from vaultapp import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('about/',views.about),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
